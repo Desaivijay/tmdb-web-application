4299279c870764b4c170ca970d0b6bbeddea61f2
Vijay desai <desaivijay918@gmail.com>
2023-08-11 21:31:17 -0400

    finel

--- Program.cs
+++ Program.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,8 +30,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
-
-// ... rest of your code
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
